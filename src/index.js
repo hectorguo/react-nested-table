8fc18dc5d6da02d35fe9206239bb50aacab3b4d0
Hector Guo <hectorguo@live.com>
2017-12-28 17:01:45 -0800

    add react-nested-table

--- src/index.js
+++ src/index.js
@@ -1,9 +1,219 @@
-import React, {Component} from 'react'
+import "react-table/react-table.css";
+import './style.scss';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactTable from 'react-table';
+import ImageZoom from 'react-medium-image-zoom';
+import _ from 'lodash';
+
+
+const MIN_ROWS = 0;
+const PAGE_SIZE = 50;
+
+function uniqId() {
+    return `${Date.now()}${Math.random() * 10 | 0}`.substr(5);
 }
+
+/**
+ * Get the width of text, used for adjusting column width on a table
+ * @param {String} text the text that wants to get its width
+ * @param {String} font the font style of current text
+ * @returns {Number} the width (pixel)
+ */
+function getTextWidth(text, font = 'bold 18px sans-serif') {
+    const minWidth = 25;
+    // re-use canvas object for better performance
+    const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+    const context = canvas.getContext("2d");
+    context.font = font;
+    const metrics = context.measureText(text);
+    return Math.max(metrics.width, minWidth);
+}
+
+function isArray(val) {
+    return Array.isArray(val);
+}
+
+function isObject(val) {
+    return val && typeof val === 'object' && val.constructor === Object;
+}
+
+function isImage(val) {
+    if (typeof val !== 'string') {
+        return false;
+    }
+    const fileExtension = val.split('.').pop();
+    const IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff'];
+    return IMAGE_EXTENSIONS.indexOf(fileExtension) > -1;
+}
+
+function isHtml(val) {
+    return typeof val === 'string' && val.split('.').pop().includes('htm');
+}
+
+// Mapping between data key and column header title for display
+let keyMaps = {};
+
+// Mapping between data key and data model (schema for columns)
+let dataModelMaps = new Map();
+
+// used for storing expanding keys in different rows
+let currentExpandedKeys = {};
+
+/**
+ * Render a table/iframe/list based on data
+ * It will automatically change UI by the types of data
+ * @param {Array/Object/String} data data needs to be rendered
+ */
+function renderByData(data) {
+    if (!data) {
+        return null;
+    }
+
+    if (isHtml(data)) {
+        return <iframe src={data} style={{ height: "50vh", width: "100%" }} />;
+    }
+
+    if (isObject(data)) {
+        data = [data];
+    }
+
+    if (isArray(data)) {
+        const dataModel = data[0];
+
+        // render as a list if there is only String or Number in Array
+        if (!isObject(dataModel) && !isArray(dataModel)) {
+            return <ul>
+                {data.map((item, i) => <li key={`nested_table_${i}`}>{item}</li>)}
+            </ul>;
+        }
+
+        const columns = dataToColumns(data);
+
+        return <ReactTable
+            data={data}
+            columns={columns}
+            className="-striped -highlight"
+            minRows={0}
+            defaultPageSize={PAGE_SIZE}
+            showPagination={data && data.length > PAGE_SIZE ? true : false}
+            SubComponent={(row) => {
+                // get current active key which needs to be expanded (triggered by clicking on a td element)
+                const currentExpandedKey = currentExpandedKeys[row.index];
+
+                return <div className="react-nested-table-inner">
+                    <h4 className="title">{keyMaps[currentExpandedKey] || currentExpandedKey}</h4>
+                    {renderByData(row.original[currentExpandedKey])}
+                </div>
+            }}
+            // onExpandedChange={(newExpanded, index, event) => {console.log('onExpand', newExpanded, index, event)}}
+            getTdProps={(state, rowInfo, column, instance) => {
+                return {
+                    onClick: (e, handleOriginal) => {
+                        // used to identify which column is expanding
+                        currentExpandedKeys[rowInfo.index] = column.id;
+                        // IMPORTANT! React-Table uses onClick internally to trigger
+                        // events like expanding SubComponents and pivots.
+                        // By default a custom 'onClick' handler will override this functionality.
+                        // If you want to fire the original onClick handler, call the
+                        // 'handleOriginal' function.
+                        if (handleOriginal) {
+                            handleOriginal()
+                        }
+                    }
+                }
+            }}
+        />
+    }
+}
+
+/**
+ * Transfer data to columns that is used for rendering table
+ * @param {Array} data 
+ * @returns {Array} an array of columns configuration
+ */
+function dataToColumns(data) {
+    if (data && data.length) {
+        const dataModel = data[0];
+
+        return Object.keys(dataModel).map(key => {
+            const currentData = dataModel[key];
+            // if (!currentData || !currentData.length) {
+            //     return {
+            //         Header: keyMaps[key] || key,
+            //         accessor: key,
+            //         Cell: cellData => (
+            //             null
+            //         )
+            //     };
+            // }
+
+            if (isObject(currentData) || isArray(currentData) || isHtml(currentData)) {
+                return {
+                    expander: true,
+                    Header: () => <strong>{keyMaps[key] || key}</strong>,
+                    width: 65,
+                    id: key,
+                    Expander: ({ isExpanded, ...rowInfo }) =>
+                        <div>
+                            {isExpanded && currentExpandedKeys[rowInfo.index] === key
+                                ? <span>&#x2299;</span>
+                                : <span>&#x2295;</span>}
+                        </div>,
+                    style: {
+                        cursor: "pointer",
+                        fontSize: 25,
+                        padding: "0",
+                        textAlign: "center",
+                        userSelect: "none"
+                    }
+                };
+            }
+
+            if (isImage(currentData)) {
+                return {
+                    Header: keyMaps[key] || key,
+                    accessor: key,
+                    Cell: cellData => (
+                        <ImageZoom
+                            image={{
+                                src: cellData.original[key],
+                                alt: key,
+                                className: 'image'
+                            }}
+                        />
+                    )
+                }
+            }
+
+            return {
+                Header: keyMaps[key] || key,
+                accessor: key,
+                width: getTextWidth(key)
+            };
+        });
+
+    }
+    return [{ Header: 'ID', accessor: 'id' }];
+
+}
+
+class ReactNestedTable extends Component {
+    render() {
+        return renderByData(this.props.data);
+    }
+}
+
+ReactNestedTable.defaultProps = {
+    data: []
+}
+
+ReactNestedTable.propTypes = {
+    /**
+     * data, should be a json
+     */
+    data: PropTypes.array.isRequired,
+}
+
+export default ReactNestedTable;
